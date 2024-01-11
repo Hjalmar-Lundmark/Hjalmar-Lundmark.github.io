cd2ba985faafbdd5b698c0a413b899c235a5a933
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2024-01-11 09:09:43 +0100

    Cleanup js

--- js/image.js
+++ js/image.js
@@ -1,29 +1,15 @@
-// let imageBox1 = document.getElementById("imageBox1");
 let modalImage = document.getElementById("modal-image");
 let modal = document.getElementById("myModal");
 let span = document.getElementsByClassName("close")[0];
 let body = document.getElementById("body");
 
-function getImg(smallImg) {
-    // imageBox1.src = smallImg.src;
+function openImg(smallImg) {
     let src = smallImg.src;
     modal.style.display = "flex";
     modalImage.src = src;
 
     body.style.overflow = "hidden";
 }
-
-// Get the modal image tag
-
-// Get the <span> element that closes the modal
-
-
-// When the user clicks the big picture, set the image and open the modal
-// imageBox1.onclick = function (e) {
-//     var src = e.srcElement.src;
-//     modal.style.display = "block";
-//     modalImage.src = src;
-// };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
